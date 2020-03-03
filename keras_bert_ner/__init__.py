284378a686bb82386cd6a07ac25ce11344320cc5
liushaoweihua <liushaoweihua@126.com>
2020-03-03 16:26:39 +0800

    add __init__.py

---
+++ keras_bert_ner/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Author: Shaoweihua.Liu
+@Contact: liushaoweihua@126.com
+@Site: github.com/liushaoweihua
+@File: __init__.py
+@Time: 2020/3/3 11:52 PM
+"""
---
+++ keras_bert_ner/utils/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Author: Shaoweihua.Liu
+@Contact: liushaoweihua@126.com
+@Site: github.com/liushaoweihua
+@File: query.py
+@Time: 2020/3/3 11:52 PM
+"""
